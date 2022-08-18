06d04d2a45bb35fb2e35b36f3d8224ddac3dd26a
Web Dev Journey <jmerino.dev@gmail.com>
2022-08-18 20:48:37 +0200

    refactor game.js

--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 var startBtn = document.getElementById('start');
 var squares = document.getElementsByClassName('square');
 var score = document.getElementsByClassName('score');
+var timeBetweenAnimations = 600;
 var userAnswers = [];
 var sequence = [];
 var sequenceLength = 1;
+
+startBtn.addEventListener('click', startGame, false);
 
 function startGame() {
     hideInstructions();
@@ -18,43 +21,34 @@ function startGame() {
             squares[obj].classList.add('flashy');
             squares[obj].setAttribute('data-id', obj);
             previousIndex = obj;
-        }, 500 * (index + 1));
+        }, timeBetweenAnimations * (index + 1));
     });
     setTimeout(function() {
         for (var i = 0; i < squares.length; i++) {
             squares[i].addEventListener('click', registerUserClick, false);
             squares[i].style.cursor = "pointer";
         }
-    }, (sequenceLength + 1) * 500);
+    }, (sequenceLength + 1) * timeBetweenAnimations);
 }
 
 function registerUserClick() {
     var squareId = this.getAttribute('data-id');
-    squareId = (squareId == null) ? squareId = -1 : squareId;
-    userAnswers.push(Number(squareId));
+    (squareId == null) ? resetGame() : userAnswers.push(Number(squareId));
+
     if (userAnswers.length == sequence.length) {
         checkAnswers();
     }
 }
 
 function checkAnswers() {
-    var correct = compareArrays(userAnswers, sequence);
-    if (correct) {
+    if (compareArrays(userAnswers, sequence)) {
         score[0].innerHTML = Number(score[0].innerHTML) + 1;
         userAnswers = [];
-        for (var i = 0; i < squares.length; i++) {
-            squares[i].removeEventListener('click', registerUserClick, false);
-            squares[i].style.cursor = "default";
-            squares[i].classList.remove('flashy');
-        }
         sequenceLength++;
+        resetSquares();
         startGame();
     }
     else {
-        var title = document.querySelector('.level');
-        title.innerHTML = "GAME OVER";
-        userAnswers = [];
-        sequenceLength = 1;
         resetGame();
     }
 }
@@ -68,31 +62,25 @@ function compareArrays(a, b) {
 }
 
 function randomSequence(num) {
-    let randomSequence = [];
+    let sequence = [];
     for (var i = 0; i < num; i++) {
-        randomSequence.push(Math.floor(Math.random() * (6 - 0 + 0) + 0));
+        sequence.push(Math.floor(Math.random() * (6 - 0 + 0) + 0));
     }
-    console.log(randomSequence);
-    return randomSequence;
+    return sequence;
 }
 
 function hideInstructions() {
-    var instructions = document.querySelector('.instructions');
-    instructions.style.display = "none";
-    var currentLevel = document.querySelector('.current-level');
-    currentLevel.style.display = "inline";
+    document.querySelector('.instructions').style.display = "none";
+    document.querySelector('.current-level').style.display = "inline";
 }
 
 function showInstructions() {
-    var instructions = document.querySelector('.instructions');
-    instructions.style.display = "block";
-    var currentLevel = document.querySelector('.current-level');
-    currentLevel.style.display = "none";
+    document.querySelector('.instructions').style.display = "block";
+    document.querySelector('.current-level').style.display = "none";
 }
 
 function resetLevelHeading() {
-    var title = document.querySelector('.level');
-    title.innerHTML = "Level <span class='score'>1</span>";
+    document.querySelector('.level').innerHTML = "Level <span class='score'>1</span>";
 }
 
 function resetSquares() {
@@ -105,11 +93,12 @@ function resetSquares() {
 }
 
 function resetGame() {
+    document.querySelector('.level').innerHTML = "GAME OVER";
+    userAnswers = [];
+    sequenceLength = 1;
     setTimeout(function() {
         showInstructions();
         resetLevelHeading();
         resetSquares();
     }, 2000);
 }
-
-startBtn.addEventListener('click', startGame, false);
